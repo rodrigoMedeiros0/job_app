1bae746cc14aebe4689591a4e8939203fe6340a9
Rodrigo <rodrigovinicius.bsb@gmail.com>
2024-01-16 09:55:27 -0300

    url job detail teste

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello World!!")
+
+def job_detail(request, id):
+    return HttpResponse(f"Job Detail Page: {id}")
