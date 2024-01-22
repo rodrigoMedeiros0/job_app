9ea9ad1482a6c94484e8954d508d887d678461bd
Rodrigo <rodrigovinicius.bsb@gmail.com>
2024-01-22 12:19:46 -0300

    odule tempalte completed

--- app/views.py
+++ app/views.py
@@ -22,23 +22,27 @@ class TempClass:
     x = 5
 
 def job_list(request):
-    list_of_jobs = "<ul>"
-    for j in job_title:
-        job_id = job_title.index(j)
-        detail_url = reverse('jobs_detail', args=(job_id,))
-        list_of_jobs += f"<li><a href='{detail_url}'>{j}</a></li>"
-    list_of_jobs += "</ul>"
-    return HttpResponse(list_of_jobs)
+    # list_of_jobs = "<ul>"
+    # for j in job_title:
+    #     job_id = job_title.index(j)
+    #     detail_url = reverse('jobs_detail', args=(job_id,))
+    #     list_of_jobs += f"<li><a href='{detail_url}'>{j}</a></li>"
+    # list_of_jobs += "</ul>"
+    # return HttpResponse(list_of_jobs)
+    context={"list_of_jobs": job_title} 
+    return render(request, "app/index.html", context)
 
 
 def job_detail(request, id):
-    print(type(id))
-    
+    # print(type(id))
+
     try:
         if id == 0:
             return redirect(reverse('jobs_home'))
-        return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
-        return HttpResponse(return_html) 
+        # return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
+        # return HttpResponse(return_html) ,
+        context={"job_title": job_title[id], "job_description": job_description[id]}
+        return render(request, "app/job_detail.html", context)
     except:
         return HttpResponseNotFound("Page not found.")
     
