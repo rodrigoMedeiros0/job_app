ba1951f278efc32522f5106f3fbf8ab6b8a7b1e4
Rodrigo <rodrigovinicius.bsb@gmail.com>
2024-01-26 11:20:24 -0300

    pushing datas from database

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.template import loader
+from app.models import JobPost
 
 job_title = [
     "First Job",
@@ -22,26 +23,18 @@ class TempClass:
     x = 5
 
 def job_list(request):
-    # list_of_jobs = "<ul>"
-    # for j in job_title:
-    #     job_id = job_title.index(j)
-    #     detail_url = reverse('jobs_detail', args=(job_id,))
-    #     list_of_jobs += f"<li><a href='{detail_url}'>{j}</a></li>"
-    # list_of_jobs += "</ul>"
-    # return HttpResponse(list_of_jobs)
-    context={"list_of_jobs": job_title} 
+    jobs = JobPost.objects.all()
+    context={"jobs": jobs} 
     return render(request, "app/index.html", context)
 
 
 def job_detail(request, id):
-    # print(type(id))
 
     try:
-        if id == 0:
+        if id == 1:
             return redirect(reverse('jobs_home'))
-        # return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
-        # return HttpResponse(return_html) ,
-        context={"job_title": job_title[id], "job_description": job_description[id]}
+        job = JobPost.objects.get(id=id)
+        context={"job": job }
         return render(request, "app/job_detail.html", context)
     except:
         return HttpResponseNotFound("Page not found.")
